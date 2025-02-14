1d336f8cc8c4da8bd9d53579ad58397f13b9f979
DrewCudmore <drew.cudmore@memoryanchor.com>
2025-02-14 08:53:38 -0600

    Initial Commit

---
+++ TastExchange/TastExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastExchangeApp.swift
+//  TastExchange
+//
+//  Created by Drew Cudmore on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TastExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TastExchangeTests/TastExchangeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TastExchangeTests.swift
+//  TastExchangeTests
+//
+//  Created by Drew Cudmore on 2/14/25.
+//
+
+import Testing
+@testable import TastExchange
+
+struct TastExchangeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
